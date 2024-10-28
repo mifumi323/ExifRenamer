0eff01b1c3ee232976535bbb4a864ff9906c7e1c
mifumi323 <mifumi323@gmail.com>
2024-10-28 21:57:01 +0900

    リネームのロジック本体を別クラスに分離

--- ExifRenamer/MainWindow.xaml.cs
+++ ExifRenamer/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Windows;
-using System.Windows.Media.Imaging;
 
 namespace ExifRenamer
 {
@@ -38,7 +37,7 @@ namespace ExifRenamer
                 var pattern = txtOutputPath.Text;
                 foreach (var inputFilePath in inputFilePaths)
                 {
-                    var outputFilePath = TranslatePath(inputFilePath, pattern);
+                    var outputFilePath = Renamer.TranslatePath(inputFilePath, pattern);
                     txtLog.Text += $"in: {inputFilePath}\nout: {outputFilePath}\n";
                     if (!string.IsNullOrEmpty(outputFilePath))
                     {
@@ -53,41 +52,6 @@ namespace ExifRenamer
                     }
                 }
                 e.Handled = true;
-            }
-        }
-
-        private static string? TranslatePath(string inputFilePath, string pattern)
-        {
-            try
-            {
-                using var stream = new MemoryStream(File.ReadAllBytes(inputFilePath));
-                var frame = BitmapFrame.Create(stream);
-                if (frame.Metadata is not BitmapMetadata metadata)
-                {
-                    return null;
-                }
-                var dateString = metadata.DateTaken;
-                var date = DateTime.Parse(dateString);
-
-                return pattern
-                    .Replace("<yyyy>", date.Year.ToString("0000"))
-                    .Replace("<yy>", (date.Year % 100).ToString("00"))
-                    .Replace("<y>", date.Year.ToString())
-                    .Replace("<MM>", date.Month.ToString("00"))
-                    .Replace("<M>", date.Month.ToString())
-                    .Replace("<dd>", date.Day.ToString("00"))
-                    .Replace("<d>", date.Day.ToString())
-                    .Replace("<HH>", date.Hour.ToString("00"))
-                    .Replace("<H>", date.Hour.ToString())
-                    .Replace("<mm>", date.Minute.ToString("00"))
-                    .Replace("<m>", date.Minute.ToString())
-                    .Replace("<ss>", date.Second.ToString("00"))
-                    .Replace("<s>", date.Second.ToString())
-                    ;
-            }
-            catch (Exception)
-            {
-                return null;
             }
         }
     }
---
+++ ExifRenamer/Renamer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace ExifRenamer
+{
+    public class Renamer
+    {
+        public static string? TranslatePath(string inputFilePath, string pattern)
+        {
+            try
+            {
+                using var stream = new MemoryStream(File.ReadAllBytes(inputFilePath));
+                var frame = BitmapFrame.Create(stream);
+                if (frame.Metadata is not BitmapMetadata metadata)
+                {
+                    return null;
+                }
+                var dateString = metadata.DateTaken;
+                var date = DateTime.Parse(dateString);
+
+                return pattern
+                    .Replace("<yyyy>", date.Year.ToString("0000"))
+                    .Replace("<yy>", (date.Year % 100).ToString("00"))
+                    .Replace("<y>", date.Year.ToString())
+                    .Replace("<MM>", date.Month.ToString("00"))
+                    .Replace("<M>", date.Month.ToString())
+                    .Replace("<dd>", date.Day.ToString("00"))
+                    .Replace("<d>", date.Day.ToString())
+                    .Replace("<HH>", date.Hour.ToString("00"))
+                    .Replace("<H>", date.Hour.ToString())
+                    .Replace("<mm>", date.Minute.ToString("00"))
+                    .Replace("<m>", date.Minute.ToString())
+                    .Replace("<ss>", date.Second.ToString("00"))
+                    .Replace("<s>", date.Second.ToString())
+                    ;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
